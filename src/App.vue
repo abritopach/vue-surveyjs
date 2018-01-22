ffa843b56ea02ba58890667441251d3075f8d366
Adrián Brito Pacheco <adrbrpa1988@gmail.com>
2018-01-22 18:39:10 +0100

    Added survey service

--- src/App.vue
+++ src/App.vue
@@ -18,6 +18,29 @@
       <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
       <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
     </ul>
+
+    <v-layout row>
+    <v-flex xs12 sm6 offset-sm3>
+      <v-card>
+        <v-list two-line>
+          <template v-for="item in items">
+            <v-subheader v-if="item.header" v-text="item.header"></v-subheader>
+            <v-divider v-else-if="item.divider" v-bind:inset="item.inset"></v-divider>
+            <v-list-tile avatar v-else v-bind:key="item.title">
+              <v-list-tile-avatar>
+                <img v-bind:src="item.avatar">
+              </v-list-tile-avatar>
+              <v-list-tile-content>
+                <v-list-tile-title v-html="item.title"></v-list-tile-title>
+                <v-list-tile-sub-title v-html="item.subtitle"></v-list-tile-sub-title>
+              </v-list-tile-content>
+            </v-list-tile>
+          </template>
+        </v-list>
+      </v-card>
+    </v-flex>
+  </v-layout>
+
     <app-footer></app-footer>
   </div>
 </template>
@@ -28,6 +51,7 @@ import Vue from 'vue'
 import Component from 'vue-class-component'
 import Toolbar from './components/common/Toolbar.vue'  
 import AppFooter from './components/common/AppFooter.vue'
+import { surveyService } from './services/survey.service'
 
 @Component({
   components: {
@@ -40,10 +64,26 @@ import AppFooter from './components/common/AppFooter.vue'
 export default class App extends Vue {
 
   msg: string;
+  items: any;
 
   constructor() {
     super();
     this.msg = 'Welcome to Your Vue.js App';
+    this.items = [
+          { header: 'Today' },
+          { avatar: 'http://www.redcresearch.ie/wp-content/uploads/2015/12/30.png', title: 'Brunch this weekend?', subtitle: "<span class='grey--text text--darken-2'>Ali Connors</span> — I'll be in your neighborhood doing errands this weekend. Do you want to hang out?" },
+          { divider: true, inset: true },
+          { avatar: 'http://www.redcresearch.ie/wp-content/uploads/2015/12/30.png', title: 'Summer BBQ <span class="grey--text text--lighten-1">4</span>', subtitle: "<span class='grey--text text--darken-2'>to Alex, Scott, Jennifer</span> — Wish I could come, but I'm out of town this weekend." },
+          { divider: true, inset: true },
+          { avatar: 'http://www.redcresearch.ie/wp-content/uploads/2015/12/30.png', title: 'Oui oui', subtitle: "<span class='grey--text text--darken-2'>Sandra Adams</span> — Do you have Paris recommendations? Have you ever been?" }
+        ];
+    this.getActiveSurveys();
+  }
+
+  getActiveSurveys() {
+    surveyService.getActiveSurveys().then(response => {
+      console.log(response)
+    })
   }
 }
 </script>
---
+++ src/services/survey.service.ts
@@ -0,0 +1,21 @@
+import axios from 'axios';
+
+class SurveyService {
+
+    private readonly api = 'https://dxsurvey.com/api/MySurveys/';
+    private readonly ownerId: string = encodeURI("adrbrpa1988@gmail.com");
+    private readonly accessKey: string = "f47de3af88e344d6a27300a2f2e4a7c1";
+
+    constructor() {
+        console.log("Hello SurveyService");
+    }
+
+    // Returns the list of active surveys.
+    getActiveSurveys() {
+        return axios.get(this.api + 'getActive?ownerId=' + this.ownerId + '&accessKey=' + this.accessKey);
+    }
+}
+
+// Export a singleton instance in the global namespace.
+export const surveyService = new SurveyService();
+
