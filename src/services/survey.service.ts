42591f00900d390ac76cb73aa504988f9723d638
Adrián Brito Pacheco <adrbrpa1988@gmail.com>
2018-02-15 17:32:13 +0100

    Updated

--- src/services/survey.service.ts
+++ src/services/survey.service.ts
@@ -3,8 +3,8 @@ import axios from 'axios';
 class SurveyService {
 
     private readonly api = 'https://dxsurvey.com/api/MySurveys/';
-    private readonly ownerId: string = encodeURI("adrbrpa1988@gmail.com");
-    private readonly accessKey: string = "9e68b59e90e1425c9b58d64a352b0d00";
+    private readonly ownerId: string = encodeURI("ownerId");
+    private readonly accessKey: string = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx";
 
     constructor() {
         console.log("Hello SurveyService");
